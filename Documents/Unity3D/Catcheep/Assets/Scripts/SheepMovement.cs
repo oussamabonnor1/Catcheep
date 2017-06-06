3a092c2e426b1c778e8b64fd0348d272d73063e7
Oussama BONNOR <oussamabonnor@gmail.com>
2017-06-06 17:45:56 +0100

    Added sheepDestroyer, sheepMovement fixed

--- Documents/Unity3D/Catcheep/Assets/Scripts/SheepMovement.cs
+++ Documents/Unity3D/Catcheep/Assets/Scripts/SheepMovement.cs
@@ -1,25 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class SheepMovement : MonoBehaviour
-{
-    [Range(0f,10)]
-    public int speed;
-
-    private Vector2 destination;
-
-    // Use this for initialization
-    void Start()
+    public class SheepMovement : MonoBehaviour
     {
-        Vector3 height = Camera.main.ScreenToWorldPoint(new Vector3(Screen.height, 0f, 0f));
-        destination = new Vector2(0f, transform.position.y - height.x);
-        print(destination);
-    }
+        [Range(0f,10)]
+        public int Speed;
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.Translate(destination * Time.deltaTime * speed);
+        private Vector2 destination;
+
+        // Use this for initialization
+        void Start()
+        {
+            Vector3 height = Camera.main.ScreenToWorldPoint(new Vector3(Screen.height, 0f, 0f));
+            destination = new Vector2(0f, transform.position.y - height.x);
+            print(destination);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.Translate(destination * Time.deltaTime * Speed);
+        }
     }
-}

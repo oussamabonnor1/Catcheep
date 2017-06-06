e9cd316c2fc7b780df0eda7ad6d8a5e4eefb3f2e
Oussama BONNOR <oussamabonnor@gmail.com>
2017-06-06 16:48:28 +0100

    Sheeps moving down, spawning, and having different destinations

--- Documents/Unity3D/Catcheep/Assets/Scripts/SheepMovement.cs
+++ Documents/Unity3D/Catcheep/Assets/Scripts/SheepMovement.cs
@@ -4,23 +4,22 @@ using UnityEngine;
 
 public class SheepMovement : MonoBehaviour
 {
-    // private Rigidbody2D myRigidbody2D;
+    [Range(0f,10)]
+    public int speed;
 
     private Vector2 destination;
 
     // Use this for initialization
     void Start()
     {
-        //   myRigidbody2D = GetComponent<Rigidbody2D>();
-
         Vector3 height = Camera.main.ScreenToWorldPoint(new Vector3(Screen.height, 0f, 0f));
-        print(height);
-        destination = new Vector2(transform.position.x, transform.position.y - height.x - 5);
+        destination = new Vector2(0f, transform.position.y - height.x);
+        print(destination);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, destination, Time.deltaTime);
+        transform.Translate(destination * Time.deltaTime * speed);
     }
 }
--- Documents/Unity3D/Catcheep/Assets/Scripts/gameManager.cs
+++ Documents/Unity3D/Catcheep/Assets/Scripts/gameManager.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class gameManager : MonoBehaviour {
+public class gameManager : MonoBehaviour
+{
+    public GameObject[] sheeps;
+    private bool gameOver;
+    private Vector3 edgeOfScreen;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        //edge of screen is a vector3 that holds the screens width (can't get it directly cause of Screen/World point difference)
+        edgeOfScreen = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0f));
+        gameOver = false;
+        StartCoroutine(sheepSpawner());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator sheepSpawner()
+    {
+        while (!gameOver)
+        {
+            float xPosition = Random.Range(-edgeOfScreen.x, edgeOfScreen.x) / 2;
+            Vector3 spawnPositionVector3 = new Vector3(xPosition, transform.position.y, transform.position.z);
+            yield return new WaitForSeconds(1f);
+            Instantiate(sheeps[Random.Range(0, sheeps.Length)], spawnPositionVector3, Quaternion.identity);
+        }
+    }
 }

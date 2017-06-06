381ce4b0d53bcb9c31b50467054932b507a5b019
Oussama BONNOR <oussamabonnor@gmail.com>
2017-06-06 22:12:54 +0100

    upload new script: GroupMovement (movement by group is now independent)

---
+++ Documents/Unity3D/Catcheep/Assets/Scripts/GroupMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroupMovement : MonoBehaviour {
+
+    [Range(0f, 10)]
+    public int Speed;
+
+    private Vector2 destination;
+
+    // Use this for initialization
+    void Start()
+    {
+        Vector3 height = Camera.main.ScreenToWorldPoint(new Vector3(Screen.height, 0f, 0f));
+        destination = new Vector2(0f, transform.position.y - height.x);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.Translate(destination * Time.deltaTime * Speed);
+    }
+}

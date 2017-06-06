ef81d3bb0269a9c2b3be394aa158fd9596165fd3
Oussama BONNOR <oussamabonnor@gmail.com>
2017-06-06 16:16:06 +0100

    uploading Game manager script modified sheep movement (sheep moving)

--- Documents/Unity3D/Catcheep/Assets/Scripts/SheepMovement.cs
+++ Documents/Unity3D/Catcheep/Assets/Scripts/SheepMovement.cs
@@ -2,15 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SheepMovement : MonoBehaviour {
+public class SheepMovement : MonoBehaviour
+{
+    // private Rigidbody2D myRigidbody2D;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Vector2 destination;
+
+    // Use this for initialization
+    void Start()
+    {
+        //   myRigidbody2D = GetComponent<Rigidbody2D>();
+
+        Vector3 height = Camera.main.ScreenToWorldPoint(new Vector3(Screen.height, 0f, 0f));
+        print(height);
+        destination = new Vector2(transform.position.x, transform.position.y - height.x - 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, destination, Time.deltaTime);
+    }
 }

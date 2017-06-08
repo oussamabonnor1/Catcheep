14b04e9638997ceefc62327d6cea837e5c49a351
Oussama BONNOR <oussamabonnor@gmail.com>
2017-06-08 19:12:54 +0100

    created diff methodes that spawns sheeps in geomitric formes

--- Documents/Unity3D/Catcheep/Assets/Scripts/gameManager.cs
+++ Documents/Unity3D/Catcheep/Assets/Scripts/gameManager.cs
@@ -40,26 +40,46 @@ public class gameManager : MonoBehaviour
     IEnumerator sheepSpawner()
     {
         yield return new WaitForSeconds(1f);
+
         while (!gameOver)
         {
             threeSheepyHorizontalFullScreen();
+            yield return new WaitForSeconds(3);
+
+            threeSheepyHorizontalpartScreen();
             yield return new WaitForSeconds(3);
         }
     }
 
     void threeSheepyHorizontalFullScreen()
     {
-       // float edges = edgeOfScreen.x - (3 * (sheeps[0].GetComponent<SpriteRenderer>().bounds.extents).x);
-        float xPosition = -edgeOfScreen.x + (sheeps[0].GetComponent<SpriteRenderer>().bounds.extents).x;// Random.Range(-edgeOfScreen.x, edges);
-       
+        float xPosition = -edgeOfScreen.x + (sheeps[0].GetComponent<SpriteRenderer>().bounds.extents).x;
+
         for (int i = 0; i < 3; i++)
         {
-            
             Vector3 spawnPositionVector3 = new Vector3(xPosition + (edgeOfScreen.x * 0.8f * i), transform.position.y, transform.position.z);
-
             Instantiate(sheeps[0], spawnPositionVector3, Quaternion.identity);
-            
+        } 
+    }
+
+    void threeSheepyHorizontalpartScreen()
+    {
+        // positioning the first (left) sheepy
+        // sheepy width is half the width of the sheep sprite
+        float sheepyWidth = (sheeps[0].GetComponent<SpriteRenderer>().bounds.extents).x;
+        // edges is the max left position value that a sheepy can have (screen width + concidering the sheepy width)
+        float edges = -edgeOfScreen.x + sheepyWidth;
+        // multiplied by 2 cause sheepy width is HALF of the sprite width and we dont want a sheepy showing in half on the right side J.I.C
+        float xPosition = Random.Range(edges,0f -(2 *sheepyWidth));
+
+        // finding out how much gap should be between the sheepys (randomly)
+        float gap = Random.Range(edgeOfScreen.x * 0.4f, edgeOfScreen.x * 0.7f);
+
+        //instantiating the sheepys
+        for (int i = 0; i < 3; i++)
+        {
+            Vector3 spawnPositionVector3 = new Vector3(xPosition + (gap * i), transform.position.y, transform.position.z);
+            Instantiate(sheeps[0], spawnPositionVector3, Quaternion.identity);
         }
-        
     }
 }

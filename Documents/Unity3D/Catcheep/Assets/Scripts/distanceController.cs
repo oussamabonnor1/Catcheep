03f7e255590893242cbd8b1144800cd7b3dd65ad
Oussama BONNOR <oussamabonnor@gmail.com>
2017-06-12 02:34:13 +0100

    distance collision detection working-ish

--- Documents/Unity3D/Catcheep/Assets/Scripts/distanceController.cs
+++ Documents/Unity3D/Catcheep/Assets/Scripts/distanceController.cs
@@ -5,37 +5,67 @@ using UnityEngine;
 public class distanceController : MonoBehaviour
 {
     private int speed;
+    private int slideSpeed;
 
-	// Use this for initialization
-	void Start ()
-	{
-	    speed = gameObject.GetComponent<SheepMovement>().Speed;
+    // Use this for initialization
+    void Start()
+    {
+        speed = gameObject.GetComponent<SheepMovement>().Speed;
+        slideSpeed = gameObject.GetComponent<SheepMovement>().slideSpeed;
+    }
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update()
+    {
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        
-            //so that a break happens: object must be beneths the speeding object, and a collision must be imminant in the X axis
-            float width = other.gameObject.GetComponent<SpriteRenderer>().sprite.bounds.extents.x +
-            GetComponent<SpriteRenderer>().sprite.bounds.extents.x;
+        goingDownSpeedControl(other);
 
-            float distance = Mathf.Abs(transform.position.x - other.transform.position.x);
+        if (gameObject.tag == "blacky")
+        {
+            goingSidewaysSpeedControl(other);
+        }
+    }
 
-            if (other.gameObject.transform.position.y < transform.position.y && distance < width)
-            {
-                gameObject.GetComponent<SheepMovement>().Speed = other.gameObject.GetComponent<SheepMovement>().Speed;
-            }
-        
+    void goingDownSpeedControl(Collider2D other)
+    {
+        //so that a break happens: object must be beneths the speeding object, and a collision must be imminant in the X axis
+        float width = other.gameObject.GetComponent<SpriteRenderer>().sprite.bounds.extents.x +
+                      GetComponent<SpriteRenderer>().sprite.bounds.extents.x;
+
+        float distance = Mathf.Abs(transform.position.x - other.transform.position.x);
+
+        if (other.gameObject.transform.position.y < transform.position.y && distance < width)
+        {
+            gameObject.GetComponent<SheepMovement>().Speed = other.gameObject.GetComponent<SheepMovement>().Speed;
+        }
+    }
+
+    void goingSidewaysSpeedControl(Collider2D other)
+    {
+        //so that a break happens: object must be beneths the speeding object, and a collision must be imminant in the Y axis
+        float width = other.gameObject.GetComponent<SpriteRenderer>().sprite.bounds.extents.x +
+                      GetComponent<SpriteRenderer>().sprite.bounds.extents.x;
+        float distance = Mathf.Abs(transform.position.y - other.transform.position.y);
+
+        if (distance < width)
+        {
+            gameObject.GetComponent<SheepMovement>().slideSpeed = 0;
+        }
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
-        gameObject.GetComponent<SheepMovement>().Speed = speed;
+        if (gameObject.tag == "sheepy" && other.gameObject.tag == "sheepy")
+        {
+            gameObject.GetComponent<SheepMovement>().Speed = speed;
+        }
+
+        if (gameObject.tag == "blacky")
+        {
+            gameObject.GetComponent<SheepMovement>().slideSpeed = slideSpeed;
+        }
     }
 }

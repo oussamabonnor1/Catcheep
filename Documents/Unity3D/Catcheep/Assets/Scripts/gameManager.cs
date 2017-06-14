6bb7e23648e99bfa1a8715041846929e6fa60ff3
Oussama BONNOR <oussamabonnor@gmail.com>
2017-06-14 22:41:46 +0100

    implimented code that scales the background (not part of canvas)

--- Documents/Unity3D/Catcheep/Assets/Scripts/gameManager.cs
+++ Documents/Unity3D/Catcheep/Assets/Scripts/gameManager.cs
@@ -8,6 +8,8 @@ using Random = UnityEngine.Random;
 public class gameManager : MonoBehaviour
 {
     public GameObject[] sheeps;
+    public GameObject background;
+
     private bool gameOver;
     private Vector3 edgeOfScreen;
     private GameObject scoreText;
@@ -25,6 +27,13 @@ public class gameManager : MonoBehaviour
         totalSheepsCaught = 0;
         combo = 0;
         score = 0;
+
+        if (background == null)
+        {
+            background = GameObject.Find("background");
+        }
+
+        Resize();
 
         scoreText = GameObject.Find("score");
         scoreText.GetComponent<Text>().text = "Score: " + score;
@@ -50,6 +59,31 @@ public class gameManager : MonoBehaviour
                 catchedSomething = false;
             }
         }
+    }
+
+    void Resize()
+    {
+        SpriteRenderer sr = background.GetComponent<SpriteRenderer>();
+        if (sr == null) return;
+
+        transform.localScale = new Vector3(1, 1, 1);
+
+        float width = sr.sprite.bounds.size.x;
+        float height = sr.sprite.bounds.size.y;
+
+
+        float worldScreenHeight = Camera.main.orthographicSize * 2f;
+        float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+        Vector3 xWidth = background.transform.localScale;
+        xWidth.x = worldScreenWidth / width;
+        background.transform.localScale = xWidth;
+        //transform.localScale.x = worldScreenWidth / width;
+        Vector3 yHeight = background.transform.localScale;
+        yHeight.y = worldScreenHeight / height;
+        background.transform.localScale = yHeight;
+        //transform.localScale.y = worldScreenHeight / height;
+
     }
 
     IEnumerator sheepSpawner()

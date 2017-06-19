71c5f7d0b782f178d163befe3900f23dbfbaca69
Oussama BONNOR <oussamabonnor@gmail.com>
2017-06-19 19:37:13 +0100

    help tools (net) fully integrated

--- Documents/Unity3D/Catcheep/Assets/Scripts/helpManager.cs
+++ Documents/Unity3D/Catcheep/Assets/Scripts/helpManager.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class helpManager : MonoBehaviour
 {
     public GameObject[] helpTools;
+    private bool helpUsed;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+	    helpUsed = false;
 	}
 	
 	// Update is called once per frame
@@ -18,11 +20,38 @@ public class helpManager : MonoBehaviour
 
     public void hayStack()
     {
-        Instantiate(helpTools[0], transform.position, Quaternion.identity);
+        if (!helpUsed)
+        {
+            helpUsed = true;
+            GameObject heyGameObject = Instantiate(helpTools[0], transform.position, Quaternion.identity);
+            StartCoroutine(helpDestroyer(3f, heyGameObject));
+        }
     }
 
     public void net()
     {
-        Instantiate(helpTools[1], transform.position, Quaternion.identity);
+        if (!helpUsed)
+        {
+            helpUsed = true;
+            GameObject neGameObject = Instantiate(helpTools[1], transform.position, Quaternion.identity);
+            StartCoroutine(helpDestroyer(1f, neGameObject));
+        }
+    }
+
+    IEnumerator helpDestroyer(float lifeTime, GameObject gameObjectToDestroy)
+    {
+        yield return new WaitForSeconds(lifeTime);
+        SpriteRenderer renderer = gameObjectToDestroy.GetComponent<SpriteRenderer>();
+
+        for (int i = 0; i < 8; i++)
+        {
+            renderer.color = new Color(renderer.color.r, renderer.color.g, renderer.color.b, 255);
+            yield return new WaitForSeconds(0.1f);
+
+            renderer.color = new Color(renderer.color.r, renderer.color.g, renderer.color.b, 10);
+            yield return new WaitForSeconds(0.05f);
+        }
+        Destroy(gameObjectToDestroy);
+        helpUsed = false;
     }
 }

bc2250d1e48319a6f7e0f1195b049fda72d882ee
Oussama BONNOR <oussamabonnor@gmail.com>
2017-06-21 01:59:39 +0100

    working on help tools drag and drop

--- Documents/Unity3D/Catcheep/Assets/Scripts/helpManager.cs
+++ Documents/Unity3D/Catcheep/Assets/Scripts/helpManager.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class helpManager : MonoBehaviour
 {
     public GameObject[] helpTools;
+    public GameObject[] helpButtons;
+
+    private GameObject hayGameObject;
+    private GameObject netGameObject;
+
     private bool helpUsed;
 
 	// Use this for initialization
@@ -14,26 +20,52 @@ public class helpManager : MonoBehaviour
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	    if (Input.GetMouseButton(0) && GameObject.FindWithTag("net"))
-	    {
-	        GameObject.FindWithTag("net").transform.position = Vector3.Lerp(GameObject.FindWithTag("net").transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), Time.deltaTime * 5);
-	    }
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && GameObject.FindWithTag("net"))
+        {
+            GameObject.FindWithTag("net").transform.position =
+                Vector3.Lerp(GameObject.FindWithTag("net").transform.position,
+                    Camera.main.ScreenToWorldPoint(Input.mousePosition), Time.deltaTime * 5);
+        }
 
-	    if (GameObject.FindWithTag("hayStack"))
-	    {
-	        //GameObject.FindWithTag("hayStack").transform.position = Vector3.Lerp(GameObject.FindWithTag("hayStack").transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), Time.deltaTime * 5);
-	    }
+        if (GameObject.FindWithTag("hayStack"))
+        {
+            //GameObject.FindWithTag("hayStack").transform.position = Vector3.Lerp(GameObject.FindWithTag("hayStack").transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), Time.deltaTime * 5);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            PointerEventData pointer = new PointerEventData(EventSystem.current);
+            
+
+            if (pointer.selectedObject == helpButtons[0])
+            {
+                hayStackDragDrop();
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0) && helpUsed)
+        {
+            hayStackCreation(hayGameObject);
+        }
     }
 
-    public void hayStack()
+    public void hayStackDragDrop()
     {
         if (!helpUsed)
         {
             helpUsed = true;
-            GameObject heyGameObject = Instantiate(helpTools[0], transform.position, Quaternion.identity);
-            StartCoroutine(helpDestroyer(5f, heyGameObject));
+            hayGameObject = Instantiate(helpTools[0], Camera.main.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity);
         }
+
+        hayGameObject.transform.position = Vector3.Lerp(hayGameObject.transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), Time.deltaTime * 5);
+    }
+
+    public void hayStackCreation(GameObject hayGameObject)
+    {
+        StartCoroutine(helpDestroyer(5f, hayGameObject));
     }
 
     public void net()
@@ -41,7 +73,7 @@ public class helpManager : MonoBehaviour
         if (!helpUsed)
         {
             helpUsed = true;
-            GameObject netGameObject = Instantiate(helpTools[1], transform.position, Quaternion.identity);
+            netGameObject = Instantiate(helpTools[1], transform.position, Quaternion.identity);
             StartCoroutine(helpDestroyer(3f, netGameObject));
         }
     }
@@ -52,4 +84,6 @@ public class helpManager : MonoBehaviour
         Destroy(gameObjectToDestroy);
         helpUsed = false;
     }
+
+    
 }

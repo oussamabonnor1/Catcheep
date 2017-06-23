7a46dabda8bbc6aa48b2fd57dfd5fbf164ccc946
Oussama BONNOR <oussamabonnor@gmail.com>
2017-06-23 02:44:36 +0100

    first menu made

---
+++ Documents/Unity3D/Catcheep/Assets/Scripts/startMenuManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startMenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void farm()
+    {
+        SceneManager.LoadScene("Farm");
+    }
+
+    public void snow()
+    {
+        SceneManager.LoadScene("snow");
+    }
+}

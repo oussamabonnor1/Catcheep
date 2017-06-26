26d6a2a5cbe7a84d3da4e8b1f65b48bc821f906b
Oussama BONNOR <oussamabonnor@gmail.com>
2017-06-26 01:23:07 +0100

    menu slider smooth effect by script added

--- Documents/Unity3D/Catcheep/Assets/Scripts/startMenuManager.cs
+++ Documents/Unity3D/Catcheep/Assets/Scripts/startMenuManager.cs
@@ -2,17 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class startMenuManager : MonoBehaviour {
+public class startMenuManager : MonoBehaviour
+{
+
+    public GameObject ScrollRect;
+
 
 	// Use this for initialization
 	void Start () {
-		
+		if( ScrollRect == null) ScrollRect = GameObject.Find("Scrollbar");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+	    if (!Input.GetMouseButton(0))
+	    {
+	        GameObject itemToScroll = GameObject.Find("seperator").transform.GetChild(1).gameObject;
+            
+            if (itemToScroll.transform.localPosition.x > 0 && ScrollRect.GetComponent<Scrollbar>().value > 0)
+            {
+	            ScrollRect.GetComponent<Scrollbar>().value =
+	                Mathf.Lerp(ScrollRect.GetComponent<Scrollbar>().value, 0, 0.05f);
+	        }
+	        else{
+                if (ScrollRect.GetComponent<Scrollbar>().value > 0)
+                {
+                    ScrollRect.GetComponent<Scrollbar>().value =
+                        Mathf.Lerp(ScrollRect.GetComponent<Scrollbar>().value, 1, 0.05f);
+                }
+            }
+        }
+
+	    if (ScrollRect.GetComponent<Scrollbar>().value > 1 || ScrollRect.GetComponent<Scrollbar>().value < 0)
+	    {
+	        print("error: " + ScrollRect.GetComponent<Scrollbar>().value);
+	    }
 	}
 
     public void farm()
@@ -29,4 +55,5 @@ public class startMenuManager : MonoBehaviour {
     {
         Application.Quit();
     }
+    
 }

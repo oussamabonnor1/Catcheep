27f0631598aa05a3f0c7465b8f4adb6e47fec71d
Oussama BONNOR <oussamabonnor@gmail.com>
2017-06-28 01:02:47 +0100

    slide menu almost completed (special case needed)

--- Documents/Unity3D/Catcheep/Assets/Scripts/startMenuManager.cs
+++ Documents/Unity3D/Catcheep/Assets/Scripts/startMenuManager.cs
@@ -13,8 +13,16 @@ public class startMenuManager : MonoBehaviour
     private Vector2 endPosition;
     private Vector2 edgeOfScreen;
 
+    //number of stops is how many images are in the menu
+    public int numOfStops;
+    //the direction of the swiping
     private bool goingRightDirection;
+    //total scroll amount (calculated using vectors and not scroll bar values)
     private float scrollAmount;
+    //the value on the scroll bar when scroll started (dah)
+    private float currentScrollValue;
+    //the destination we need to lerp to when the mouse is released and menu is mid way
+    private float desiredDestination;
 
     // Use this for initialization
     void Start()
@@ -28,12 +36,19 @@ public class startMenuManager : MonoBehaviour
         //basicaly this is true cause we always scroll right first
         goingRightDirection = true;
         scrollAmount = 0f;
+        currentScrollValue = 0f;
+        desiredDestination = -1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) startPosition = Input.mousePosition;
+        float portion = (float) 1 / (numOfStops - 1);
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPosition = Input.mousePosition;
+            currentScrollValue = ScrollBar.value;
+        }
         if (Input.GetMouseButtonUp(0)) endPosition = Input.mousePosition;
 
         if (!Input.GetMouseButton(0))
@@ -52,19 +67,59 @@ public class startMenuManager : MonoBehaviour
                 {
                     goingRightDirection = false;
                 }
-            }
-            if (ScrollBar.value > 0 && ScrollBar.value < 0.3)
-            {
-                ScrollBar.value =
-                    Mathf.Lerp(ScrollBar.value, 0, 0.05f);
+
+                if (goingRightDirection)
+                {
+                    ScrollBar.value =
+                        Mathf.Lerp(ScrollBar.value, currentScrollValue + portion, 0.05f);
+                    if(Mathf.Approximately(ScrollBar.value, currentScrollValue +portion)) {
+                        currentScrollValue
+                        = currentScrollValue + portion;
+                        print(currentScrollValue);
+                    }
+                }
+                else
+                {
+                    ScrollBar.value =
+                        Mathf.Lerp(ScrollBar.value, currentScrollValue - portion, 0.05f);
+                    if (Mathf.Approximately(ScrollBar.value, currentScrollValue + portion))
+                    {
+                        currentScrollValue
+                            = currentScrollValue - portion;
+                        print(currentScrollValue);
+                    }
+                }
             }
             else
             {
-                if (ScrollBar.value < 1)
+                //TODO fix going back for last case
+
+                //this is to determine to which portion of the menu we have to go back to
+                //made desired destination - 1 at first to not waste frames
+                if (desiredDestination == -1)
+                {
+                    for (int i = 0; i < numOfStops; i++)
+                    {
+                        //this lines means the value is between two portions of the menu (between 0 and 0.33 for exemple)
+                        if (ScrollBar.value < (i + 1) * (portion) &&
+                            ScrollBar.value > i * (portion))
+                        {
+                            //once the destination is assigned, this loop wont be accessed until we reach the destination
+                            desiredDestination = i * (portion);
+                        }
+                    }
+                }
+                else
                 {
                     ScrollBar.value =
-                        Mathf.Lerp(ScrollBar.value, 1, 0.05f);
+                        Mathf.Lerp(ScrollBar.value, desiredDestination, 0.05f);
+                    if (Mathf.Approximately(ScrollBar.value, desiredDestination))
+                    {
+                        desiredDestination = -1;
+                        print("destination reached");
+                    }
                 }
+                
             }
         }
 

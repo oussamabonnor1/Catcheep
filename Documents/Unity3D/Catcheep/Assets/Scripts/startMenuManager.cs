24c857cad5ed14b9f2accfc400b19cca9624ca2d
Oussama BONNOR <oussamabonnor@gmail.com>
2017-07-10 17:32:24 +0100

    menu slider effect code cleaned

--- Documents/Unity3D/Catcheep/Assets/Scripts/startMenuManager.cs
+++ Documents/Unity3D/Catcheep/Assets/Scripts/startMenuManager.cs
@@ -20,7 +20,7 @@ public class startMenuManager : MonoBehaviour
 
     private bool isGoingUp;
     private bool isGoingDown;
-    
+
 
     // Use this for initialization
     void Start()
@@ -28,7 +28,7 @@ public class startMenuManager : MonoBehaviour
         isGoingDown = false;
         isGoingUp = false;
 
-         portion = (float) 1 / menuCount;
+        portion = (float) 1 / menuCount;
         if (ScrollBarGameObject == null) ScrollBarGameObject = GameObject.Find("Scrollbar");
         ScrollBar = ScrollBarGameObject.GetComponent<Scrollbar>();
         edgeOfScreen = new Vector2(Screen.width, Screen.height);
@@ -38,24 +38,23 @@ public class startMenuManager : MonoBehaviour
     void Update()
     {
         //if (Input.touchCount == 0)
-         if (!Input.GetMouseButton(0))
-         {
-
-             for (int i = 1; i <= menuCount; i++)
-             {
-                 if (ScrollBar.value < ((i * portion) - (portion / 2)) && ScrollBar.value > ((i - 1) * portion))
-                 {
-                     ScrollBar.value = Mathf.Lerp(ScrollBar.value, (i - 1) * portion, 0.05f);
-                 }
-                 if (ScrollBar.value > ((i * portion) - (portion / 2)) && ScrollBar.value < (i * portion))
-                 {
-                     ScrollBar.value = Mathf.Lerp(ScrollBar.value, (i) * portion, 0.05f);
-                 }
-             }
-         }
-         if (Input.touchCount == 1)
-         {
-         }
+        if (!Input.GetMouseButton(0))
+        {
+            for (int i = 1; i <= menuCount; i++)
+            {
+                if (ScrollBar.value < ((i * portion) - (portion / 2)) && ScrollBar.value > ((i - 1) * portion))
+                {
+                    ScrollBar.value = Mathf.Lerp(ScrollBar.value, (i - 1) * portion, 0.05f);
+                }
+                if (ScrollBar.value > ((i * portion) - (portion / 2)) && ScrollBar.value < (i * portion))
+                {
+                    ScrollBar.value = Mathf.Lerp(ScrollBar.value, (i) * portion, 0.05f);
+                }
+            }
+        }
+        if (Input.touchCount == 1)
+        {
+        }
         if (isGoingUp)
         {
             ScrollBar.value = Mathf.Lerp(ScrollBar.value, destination, 0.05f);
@@ -63,7 +62,6 @@ public class startMenuManager : MonoBehaviour
         if (isGoingDown)
         {
             ScrollBar.value = Mathf.Lerp(ScrollBar.value, destination, 0.05f);
-           
         }
     }
 
@@ -71,19 +69,21 @@ public class startMenuManager : MonoBehaviour
     {
         StartCoroutine(goingUp());
     }
+
     public void goingDownButtonClick()
     {
         StartCoroutine(goingDown());
     }
 
-     IEnumerator goingUp()
+    IEnumerator goingUp()
     {
-        destination = ScrollBar.value  + portion;
+        destination = ScrollBar.value + portion;
         isGoingUp = true;
         yield return new WaitForSeconds(1f);
         isGoingUp = false;
     }
-     IEnumerator goingDown()
+
+    IEnumerator goingDown()
     {
         destination = ScrollBar.value - portion;
         isGoingDown = true;

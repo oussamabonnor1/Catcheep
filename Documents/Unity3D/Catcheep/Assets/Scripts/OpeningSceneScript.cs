20f59f306f26d7c9b8a8af0ed0cffcc487ee1ed2
Oussama BONNOR <oussamabonnor@gmail.com>
2017-07-17 01:00:16 +0100

    added opening scene script

---
+++ Documents/Unity3D/Catcheep/Assets/Scripts/OpeningSceneScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpeningSceneScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	    StartCoroutine(opening());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator opening()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene(1);
+    }
+}

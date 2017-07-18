465f6c57dbd8dcb41197014b38c80a347ef1a27f
Oussama BONNOR <oussamabonnor@gmail.com>
2017-07-18 20:12:03 +0100

    obstacles concept added

---
+++ Documents/Unity3D/Catcheep/Assets/Scripts/ObstacleController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleController : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+
+    private bool obstacleCreated;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    obstacleCreated = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator createObstacle()
+    {
+        yield return new WaitForSeconds(Random.Range(5,15));
+        GameObject obstacle = Instantiate(obstaclePrefab, obstaclePrefab.transform.position,
+            obstaclePrefab.transform.rotation);
+        
+    }
+}

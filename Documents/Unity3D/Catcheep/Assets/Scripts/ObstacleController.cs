547d1ba585aab8d9ba2c7d198e0ad1982549bb4c
Oussama BONNOR <oussamabonnor@gmail.com>
2017-07-18 20:23:51 +0100

    creating obstacle completed

--- Documents/Unity3D/Catcheep/Assets/Scripts/ObstacleController.cs
+++ Documents/Unity3D/Catcheep/Assets/Scripts/ObstacleController.cs
@@ -6,29 +6,30 @@ public class ObstacleController : MonoBehaviour
 {
     public GameObject obstaclePrefab;
     private GameObject obstacle;
-
-    private bool obstacleCreated;
+    
     private Vector2 edgeOfScreen;
 
 	// Use this for initialization
 	void Start ()
 	{
 	    edgeOfScreen = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height,0f));
-	    obstacleCreated = false;
 	    StartCoroutine(createObstacle());
 	}
 	
 	// Update is called once per frame
 	void Update () {
         
-	    if (obstacle != null && obstacleCreated) //this is an ensurance policy, do not touch it
+	    if (obstacle != null) //this is an ensurance policy, do not touch it
 	    {
-            //
+            //making the obstacle go...
 	        obstacle.transform.position += Time.deltaTime * new Vector3(1f,0f,0f);
+            //destroying it when it reaches far end of screen...
 	        if (obstacle.transform.position.x >= edgeOfScreen.x +
 	            obstacle.GetComponent<SpriteRenderer>().sprite.bounds.size.x)
 	        {
-	            
+	            Destroy(obstacle);
+                //making sure we restart the creation AFTER we destroyed the first one
+	            StartCoroutine(createObstacle());
 	        }
 	    }
 	}
@@ -38,7 +39,5 @@ public class ObstacleController : MonoBehaviour
         yield return new WaitForSeconds(Random.Range(5,15));
         obstacle = Instantiate(obstaclePrefab, obstaclePrefab.transform.position,
             obstaclePrefab.transform.rotation);
-        obstacleCreated = true;
-
     }
 }

ece43f5d98bdbc85967f57edf47751f3e0683644
Oussama BONNOR <oussamabonnor@gmail.com>
2017-08-04 00:50:05 +0100

    ship going right function created

--- Documents/Unity3D/Catcheep/Assets/Scripts/AlienManager.cs
+++ Documents/Unity3D/Catcheep/Assets/Scripts/AlienManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class AlienManager : MonoBehaviour
 {
@@ -34,9 +35,7 @@ public class AlienManager : MonoBehaviour
             yield return new WaitForSeconds(0.02f);
         } while ((int) spaceShip.transform.localPosition.y > (int) destination.y + 1);
         
-        yield return new WaitForSeconds(1f);
-        print("started");
-        StartCoroutine(shipLeaving());
+        StartCoroutine(shipGoingRight());
     }
 
     IEnumerator shipLeaving()
@@ -51,9 +50,17 @@ public class AlienManager : MonoBehaviour
         Destroy(spaceShip.gameObject);
     }
 
-    public void shipGoingRight()
+    public IEnumerator shipGoingRight()
     {
-        
+        Vector3 destination = new Vector3(edgeOfScreen.x + alienShip.GetComponentInChildren<Image>().sprite.bounds.size.x, spaceShip.transform.localPosition.y,
+            spaceShip.transform.localPosition.z);
+        //print(spaceShip.transform.localPosition.x +" / " + spaceShip.transform.position.x);
+       // destination = Camera.main.WorldToScreenPoint(destination);
+        do
+        {
+            spaceShip.transform.localPosition = new Vector3(spaceShip.transform.localPosition.x + 10, spaceShip.transform.localPosition.y, spaceShip.transform.localPosition.z);
+            yield return new WaitForSeconds(0.01f);
+        } while ((int) spaceShip.transform.position.x < (int) destination.x * 1.5f);
     }
 
     public void startMenu()

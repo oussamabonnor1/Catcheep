09649c24162e7c26e06d110e3a12ea606c60d1a8
Oussama BONNOR <oussamabonnor@gmail.com>
2017-08-04 01:00:58 +0100

    spaceship going left function completed

--- Documents/Unity3D/Catcheep/Assets/Scripts/AlienManager.cs
+++ Documents/Unity3D/Catcheep/Assets/Scripts/AlienManager.cs
@@ -6,22 +6,22 @@ using UnityEngine.UI;
 
 public class AlienManager : MonoBehaviour
 {
-
     public GameObject alienShip;
 
     private GameObject spaceShip;
     private Vector2 edgeOfScreen;
 
-	// Use this for initialization
-	void Start () {
-		edgeOfScreen = new Vector2(Screen.width,Screen.height);
-	    StartCoroutine(alienSpawner());
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        edgeOfScreen = new Vector2(Screen.width, Screen.height);
+        StartCoroutine(alienSpawner());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
 
     IEnumerator alienSpawner()
     {
@@ -31,11 +31,12 @@ public class AlienManager : MonoBehaviour
         Vector3 destination = new Vector3(alienShip.transform.localPosition.x, -edgeOfScreen.y * 0.05f, 0f);
         do
         {
-            spaceShip.transform.localPosition = Vector3.Lerp(spaceShip.transform.localPosition, destination, 2.5f * Time.deltaTime);
+            spaceShip.transform.localPosition =
+                Vector3.Lerp(spaceShip.transform.localPosition, destination, 2.5f * Time.deltaTime);
             yield return new WaitForSeconds(0.02f);
         } while ((int) spaceShip.transform.localPosition.y > (int) destination.y + 1);
-        
-        StartCoroutine(shipGoingRight());
+
+        StartCoroutine(shipGoingLeft());
     }
 
     IEnumerator shipLeaving()
@@ -43,7 +44,8 @@ public class AlienManager : MonoBehaviour
         Vector3 destination = new Vector3(alienShip.transform.localPosition.x, edgeOfScreen.y, 0f);
         do
         {
-            spaceShip.transform.localPosition = Vector3.Lerp(spaceShip.transform.localPosition, destination, 2.5f * Time.deltaTime);
+            spaceShip.transform.localPosition =
+                Vector3.Lerp(spaceShip.transform.localPosition, destination, 2.5f * Time.deltaTime);
             yield return new WaitForSeconds(0.02f);
         } while (spaceShip.transform.localPosition.y < destination.y);
 
@@ -52,15 +54,32 @@ public class AlienManager : MonoBehaviour
 
     public IEnumerator shipGoingRight()
     {
-        Vector3 destination = new Vector3(edgeOfScreen.x + alienShip.GetComponentInChildren<Image>().sprite.bounds.size.x, spaceShip.transform.localPosition.y,
+        //spaceShip.transform.localRotation = new Quaternion(spaceShip.transform.localRotation.x, spaceShip.transform.rotation.y, -5, spaceShip.transform.rotation.w);
+        Vector3 destination = new Vector3(
+            edgeOfScreen.x + alienShip.GetComponentInChildren<Image>().sprite.bounds.size.x,
+            spaceShip.transform.localPosition.y,
             spaceShip.transform.localPosition.z);
-        //print(spaceShip.transform.localPosition.x +" / " + spaceShip.transform.position.x);
-       // destination = Camera.main.WorldToScreenPoint(destination);
         do
         {
-            spaceShip.transform.localPosition = new Vector3(spaceShip.transform.localPosition.x + 10, spaceShip.transform.localPosition.y, spaceShip.transform.localPosition.z);
+            spaceShip.transform.localPosition = new Vector3(spaceShip.transform.localPosition.x + 10,
+                spaceShip.transform.localPosition.y, spaceShip.transform.localPosition.z);
             yield return new WaitForSeconds(0.01f);
-        } while ((int) spaceShip.transform.position.x < (int) destination.x * 1.5f);
+        } while ((int) spaceShip.transform.position.x < (int) destination.x * 1.05f);
+    }
+
+    public IEnumerator shipGoingLeft()
+    {
+        //spaceShip.transform.localRotation = new Quaternion(spaceShip.transform.localRotation.x, spaceShip.transform.rotation.y, -5, spaceShip.transform.rotation.w);
+        Vector3 destination = new Vector3(
+            -edgeOfScreen.x - alienShip.GetComponentInChildren<Image>().sprite.bounds.size.x,
+            spaceShip.transform.localPosition.y,
+            spaceShip.transform.localPosition.z);
+        do
+        {
+            spaceShip.transform.localPosition = new Vector3(spaceShip.transform.localPosition.x - 10,
+                spaceShip.transform.localPosition.y, spaceShip.transform.localPosition.z);
+            yield return new WaitForSeconds(0.01f);
+        } while ((int) spaceShip.transform.position.x > (int) destination.x * 1.05f);
     }
 
     public void startMenu()

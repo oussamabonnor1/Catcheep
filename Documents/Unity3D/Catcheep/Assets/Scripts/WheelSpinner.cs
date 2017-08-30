0f3806287c3e3e3939ca158d667ca494ced6fe53
Oussama BONNOR <oussamabonnor@gmail.com>
2017-08-30 19:14:57 +0100

    wheel spinning function created

--- Documents/Unity3D/Catcheep/Assets/Scripts/WheelSpinner.cs
+++ Documents/Unity3D/Catcheep/Assets/Scripts/WheelSpinner.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class WheelSpinner : MonoBehaviour
 {
@@ -10,7 +12,6 @@ public class WheelSpinner : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
-	    StartCoroutine(wheelTurned());
 	}
 	
 	// Update is called once per frame
@@ -18,16 +19,29 @@ public class WheelSpinner : MonoBehaviour
 		
 	}
 
+    public void spin()
+    {
+        StartCoroutine(wheelTurned());
+    }
+
     IEnumerator wheelTurned()
     {
-        print("whw");
-        int i = 0;
+        int i = Random.Range(5,10);
         do
         {
-            Quaternion a = wheelOfLuck.GetComponent<RectTransform>().rotation;
-            wheelOfLuck.GetComponent<RectTransform>().transform.Rotate(wheelOfLuck.transform.localPosition,i);//RotateAround(wheelOfLuck.transform.position,Vector3.back,10);
+            wheelOfLuck.GetComponent<Rigidbody2D>().AddTorque(10);
+            //wheelOfLuck.GetComponent<RectTransform>().transform.Rotate(0f,0f,10f);//RotateAround(wheelOfLuck.transform.position,Vector3.back,10);
             i++;
             yield return new WaitForSeconds(0.01f);
-        } while (i < 10);
+        } while (i < 50);
+
+        yield return new WaitForSeconds(1f);
+        do
+        {
+            wheelOfLuck.GetComponent<Rigidbody2D>().angularVelocity-=2;
+            yield return new WaitForSeconds(0.001f);
+        } while (wheelOfLuck.GetComponent<Rigidbody2D>().angularVelocity > 0);
+
+
     }
 }

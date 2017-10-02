b0d51d1677e83ebcad4654bd61d8e2039f7dc4ba
Oussama BONNOR <oussamabonnor@gmail.com>
2017-10-02 23:41:46 +0100

    not catching 20 sheeps will lead to auto loss

--- Documents/Unity3D/Catcheep/Assets/Scripts/destroyerCollider.cs
+++ Documents/Unity3D/Catcheep/Assets/Scripts/destroyerCollider.cs
@@ -1,11 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class destroyerCollider : MonoBehaviour {
+public class destroyerCollider : MonoBehaviour
+{
+    private int kills;
+    public GameObject finishPanel;
 
+    void Start()
+    {
+        kills = 0;
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
-            other.gameObject.GetComponent<sheepDestroyer>().Destruction();
+        if (other.gameObject.tag.Equals("sheepy") || other.gameObject.tag.Equals("blacky"))
+        {
+            kills++;
+        }
+        other.gameObject.GetComponent<sheepDestroyer>().Destruction();
+        if (kills >= 20)
+        {
+            gameManager.gameOver = true;
+            finishPanel.transform.GetChild(0).GetComponentInChildren<TextMeshProUGUI>().text =
+                "You Missed Too Many Sheeps !";
+        }
     }
 }

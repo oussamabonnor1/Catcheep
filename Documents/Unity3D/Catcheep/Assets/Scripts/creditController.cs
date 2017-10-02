3d4b713734168650d43d518ae2de8fc2816c7c67
Oussama BONNOR <oussamabonnor@gmail.com>
2017-10-02 15:09:56 +0100

    credit scene controller added

---
+++ Documents/Unity3D/Catcheep/Assets/Scripts/creditController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class creditController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.Escape))
+	    {
+	        SceneManager.LoadScene("Start");
+	    }
+    }
+
+    public void facebook()
+    {
+        Application.OpenURL("fb://page/344400029290078");
+    }
+
+    public void siteVisit()
+    {
+        Application.OpenURL("https://www.jetlight-studio.tk/");
+    }
+}
